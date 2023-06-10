667f85c618ba962a624b4a13676ea4d5f7a1796a
TusB <tutran19112002@gmail.com>
2023-06-10 11:53:15 +0700

    haha

--- src/components/MyProfile.tsx
+++ src/components/MyProfile.tsx
@@ -1,19 +1,33 @@
-import { SDK, useGumContext, useProfile } from '@gumhq/react-sdk';
-import { Profile } from '@gumhq/ui-components';
-import { PublicKey } from '@solana/web3.js';
-
+import { SDK, useGumContext, useProfile } from "@gumhq/react-sdk";
+import { Profile } from "@gumhq/ui-components";
+import { PublicKey } from "@solana/web3.js";
 
 export function MyProfile() {
   const { sdk } = useGumContext();
-  const { profile } = useProfile(sdk, new PublicKey("HbaeYzrgBnM8gPshvNgd2xH7mV5fpBnAo3Gqwr1xfjD2"));
-  
+  const { profile } = useProfile(
+    sdk,
+    new PublicKey("HbaeYzrgBnM8gPshvNgd2xH7mV5fpBnAo3Gqwr1xfjD2")
+  );
+
   const profileData = {
     ...profile?.metadata,
     following: profile?.following || 0,
     followers: profile?.followers || 0,
-  }
+    connect: {
+      following: false,
+      follow: () => {
+        alert("follow");
+      },
+      unfollow: () => {
+        alert("unfollow");
+      },
+    },
+  };
 
   return (
-    <Profile data={profileData} />
+    <div>
+      <Profile data={profileData} />
+      <button>Invest</button>
+    </div>
   );
 }
